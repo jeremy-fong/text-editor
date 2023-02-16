05d8b41a167ac32167c48b256642d25ad1d6537d
jeremy-fong <112743562+jeremy-fong@users.noreply.github.com>
2023-02-16 03:45:07 -0800

    added code to webpack.config.js for a service worker, manifest file, added css loaders and babel  to webpack, and added code to set up asset cache in src-sw.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,23 +1,27 @@
 import { openDB } from 'idb';
 
+const DB_NAME = 'jate';
+const OBJ_STORE = 'jate';
+const DB_VERSION = 1; 
+
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB(DB_NAME, DB_VERSION, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (db.objectStoreNames.contains(DB_NAME)) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore(OBJ_STORE, { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('PUT request to the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
+  console.log('PUT to the database');
+  const jateDb = await openDB(DB_NAME, DB_VERSION);
+  const tx = jateDb.transaction(DB_NAME, 'readwrite');
+  const store = tx.objectStore(OBJ_STORE);
   const request = store.put({ id: id, jate: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
@@ -26,9 +30,9 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('Getting all data');
-  const jateDb = await openDB('jate', 1);
-  const txt = jateDb.transaction('jate', 'readonly');
-  const store = txt.objectStore('jate');
+  const jateDb = await openDB(DB_NAME, DB_VERSION);
+  const txt = jateDb.transaction(DB_NAME, 'readonly');
+  const store = txt.objectStore(OBJ_STORE);
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
